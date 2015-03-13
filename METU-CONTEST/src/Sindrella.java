a970d011181bdbfc39a4fa7937b63cc996ac8417
Mert Arısoy <mertarisoy@gmail.com>
2015-03-13 17:50:59 +0200

    Update

--- METU-CONTEST/src/Sindrella.java
+++ METU-CONTEST/src/Sindrella.java
@@ -8,10 +8,11 @@ public class Sindrella {
 		
 		int n = scan.nextInt();
 		String numberStr = scan.next();
+		//Key must be start with first char
 		int sumOfDigits = numberStr.charAt(0) - '0';
 		String key = "" + numberStr.charAt(0);
 		
-		
+		int [] sevenRule = {3,2,-1,-3,-2,1};
 		for(int i = 1; i < n;i++){
 			
 			int divisor = numberStr.charAt(i) - '0';
@@ -28,39 +29,42 @@ public class Sindrella {
 				sumOfDigits+= c;
 				break;
 			case 4:
-				if(numberStr.length() < 2){
-					key += '4';
-					sumOfDigits+= 4;
+				c = key.charAt(key.length()-1) - '0';
+				if(c % 2 == 0){
+					key += '0';
 				}
 				else{
-					int number = key.charAt(key.length()-1) - '0';
-					if(number % 2 == 0){
-						key += '0';
-					}
-					else{
-						key += '2';
-						sumOfDigits += 2;
-					}
+					key += '2';
+					sumOfDigits += 2;
 				}
 				break;
 			case 7:
-				key += '5';
-				sumOfDigits += 5;
+				int sum = 0;
+				int kl = key.length();
+				for(int j = kl-1; j >= 0;j--){
+					sum += (key.charAt(j) - '0') * sevenRule[(kl-j-1)%6];
+				}
+				c = 0;
+				while((sum + c) % 7 != 0){
+					c++;
+				}
+				key += (char)(c + '0');
+				sumOfDigits += c;
 				break;
 			case 8:
-				if(numberStr.length() < 2){
-					key += '8';
-					sumOfDigits+= 8;
+				if(numberStr.length() < 3){
+					key += '6';
+					sumOfDigits+= 6;
 				}
 				else{
-					int number = Integer.parseInt(key.substring(key.length()-2));
-					if(number % 2 == 0){
-						key += '0';
+					c = Integer.parseInt(key.substring(key.length()-2));
+					c*= 10;
+					while(c % 8 != 0){	
+						c+=2;
 					}
-					else{
-						key += '2';
-						sumOfDigits += 2;
-					}
+					int k = c %10;
+					sumOfDigits += k;
+					key += (char)(k + '0');
 				}
 				break;
 			case 9:
@@ -68,6 +72,7 @@ public class Sindrella {
 				key += (char)(c + '0');
 				sumOfDigits+= c;
 				break;
+			
 			case 1:
 			case 2:
 			case 5:
